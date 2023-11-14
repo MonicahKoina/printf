5f6212c01844b755a511a062a9f916ed3c72b306
muhoro-wandia <61504824+Muhos-wandia@users.noreply.github.com>
2023-11-14 19:35:43 +0300

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -6,7 +6,7 @@
  * @ind: ind.
  * @buffer: Buffer array to handle print.
  * @flags: Calculates active flags
- * @width: get width.
+ * @width: gets width.
  * @precision: Precision specification
  * @size: Size specifier
  * Return: 1 or 2;
