8b2e97056db374a09472a395beac2df98deff16f
muhoro-wandia <61504824+Muhos-wandia@users.noreply.github.com>
2023-11-14 14:05:46 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -5,7 +5,7 @@ void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format.
- * Return: Printed chars.
+ * Return: The number of printed chars.
  */
 int _printf(const char *format, ...)
 {
